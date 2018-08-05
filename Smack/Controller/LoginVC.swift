76debf0b3c880f0ff5184b51faaf09495f92bae0
Pyel <pyel909@gmail.com>
2018-08-06 01:16:58 +0200

    LoginVC design done

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,28 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by pyel on 06/08/2018.
+//  Copyright © 2018 pyel. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    //Outlets
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    //Actions
+    @IBAction func closeBtnPressed(_ sender: Any){
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by pyel on 06/08/2018.
+//  Copyright © 2018 pyel. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
