55f6e7a0e8308e25c9966f46da3318b29699d41a
Pyel <pyel909@gmail.com>
2018-08-06 11:12:59 +0200

    AuthService setup

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,8 +8,23 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URL Constants
+
+let BASE_URL = "https://chattychatchatapptest.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
 //Segues
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND_TO_CHANNEL = "unwindToChannelVC"
+
+
+//User Defaults
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
